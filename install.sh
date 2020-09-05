30227067e0cfd0c015aebe09bd0ad96dcd2b35a8
Samuel Sloniker <scoopgracie@gmail.com>
2020-09-05 14:58:00 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,33 @@
 termux-setup-storage
 pkg install python
-git clone https://gist.github.com/scoopgracie/1d0680979852e8f6715e72e47bb70e44 sfstool_install
-mv sfstool_install/sfstool.sh $PREFIX/bin/sfstool
+curl -L https://raw.githubusercontent.com/scoopgracie/sfstool/master/sfstool.sh > $PREFIX/bin/sfstool
 chmod +x $PREFIX/bin/sfstool
-rm -rf sfstool_install
 echo "sfstool is installed (please set up ~/.sfs)"
-echo "sfstool comes with ABSOLUTELY NO WARRANTY, not even the implied warranties of"
-echo "merchantibility, fitness for a particular purpose, and noninfringement. The"
-echo "authors accept no liability or responsibility for any claim, damages, or other"
-echo "liability, arising from, out of, or in connection with sfstool, the use of"
-echo "sfstool, or other dealings in sfstool."
+cat << HERE
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org>
+
+sfstool may be obtained from https://github.com/scoopgracie/sfstool.
+HERE
